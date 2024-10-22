1bd753bf13af0bec4c162fedb502ba25a0a0fd19
last_name <artemdashkov@gmail.com>
2024-10-22 22:13:16 +0300

    add new code

--- example_2.py
+++ example_2.py
@@ -1,7 +1,11 @@
-import os
+import random
+list_1 = [1, 2, 3, 4, 5, 6, 7]
+a = random.sample(list_1, 3)
+list_1_len = len(list_1)
+list_2 = list(range(0, list_1_len))
+number = random.randint(1, 10)
 
-b = 0
-
-a = 5 if b == 1 else 8
-
-print(a)
+print('a\t\t\t\t', a)
+print('list_1_len\t\t', list_1_len)
+print('list_2\t\t\t', list_2)
+print('number\t\t\t', number)
